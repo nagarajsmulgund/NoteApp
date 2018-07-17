bad1593bdf0642bb907b2ec9515a548e94bfd53e
pschilakanti <nagarajsmulgund@gmail.com>
2018-07-17 19:03:41 +0530

    initial commit for NoteApp

---
+++ NoteApp/app/src/main/java/com/noteapp/network/model/BaseResponse.java
@@ -0,0 +1,10 @@
+package com.noteapp.network.model;
+
+
+public class BaseResponse {
+    String error;
+
+    public String getError() {
+        return error;
+    }
+}
---
+++ NoteApp/app/src/main/java/com/noteapp/network/model/Note.java
@@ -0,0 +1,24 @@
+package com.noteapp.network.model;
+
+
+public class Note extends BaseResponse{
+    int id;
+    String note;
+    String timestamp;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+}
